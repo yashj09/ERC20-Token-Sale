7fe218bc58dc1877b84d2f4a28a966dd7398b17d
Yash Jain <yashj8858@gmail.com>
2024-06-30 22:57:14 +0530

    init

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+// eslint-disable-next-line no-undef
+const dotenv = require('dotenv');
+dotenv.config();
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
+};
